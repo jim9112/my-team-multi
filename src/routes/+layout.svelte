fcf3e75cf3eefc1e48aaf25612c950b1b9301871
James Hannan <jim9112@gmail.com>
2024-08-26 13:36:47 -0400

    add design colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script>
   import "../app.css";
 </script>
-<header>
-  <nav>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/contact">Contact</a>
-  </nav>
-</header>
-<slot />
+<div class="min-h-screen text-white bg-police-blue">
+  <header class="flex items-center">
+    <img src="/assets/logo.svg" alt="">
+    <nav>
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+      <a href="/contact">Contact</a>
+    </nav>
+  </header>
+  <slot />
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'police-blue': '#2C6269',
+        'midnight-green': '#014E56',
+        'light-coral': '#F67E7E',
+        'rapture-blue': '#79C8C7',
+        'deep-jungle-green': '#004047',
+        'sacramento-state-green': '#012F34',
+        'dark-green': '#002529',
+      }
+    },
   },
   plugins: [],
 }
