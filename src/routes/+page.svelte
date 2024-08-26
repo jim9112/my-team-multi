fea1ef26ef0cf723c5fca79ee0f7972d251203f8
James Hannan <jim9112@gmail.com>
2024-08-26 14:23:53 -0400

    add all content into index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,60 @@
-<h1 class=''>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="flex items-end">
+  <h1 class="w-1/2 font-bold text-8xl">Find the best <span class="text-light-coral">talent</span></h1>
+  <p class="w-1/2">
+    Finding the right people and building high performing teams can 
+    be hard. Most companies aren’t tapping into the abundance of global 
+    talent. We’re about to change that.
+  </p>
+</section>
+<section class="flex bg-sacramento-state-green">
+  <h2>Build & manage distributed teams like no one else.</h2>
+  <div>
+    <div>
+      Experienced Individuals
+      Our network is made up of highly experienced professionals who are 
+      passionate about what they do.
+    </div>
+    <div>
+      Easy to Implement
+      Our processes have been refined over years of implementation meaning 
+      our teams always deliver.
+    </div>
+    <div>
+      Enhanced Productivity
+      Our customized platform with in-built analytics helps you manage your 
+      distributed teams.
+    </div>
+  </div>
+</section>
+<section>
+  <h2>
+    Delivering real results for top companies. Some of our success stories.
+  </h2>
+  <div>
+    <div>
+      “The team perfectly fit the specialized skill set required. They focused 
+      on the most essential features helping us launch the platform eight months 
+      faster than planned.”
+      Kady Baker
+      Product Manager at Bookmark
+    </div>
+    <div>
+      “We needed to automate our entire onboarding process. The team came in and 
+      built out the whole journey. Since going live, user retention has gone 
+      through the roof!”
+      Aiysha Reese
+      Founder of Manage
+    </div>
+    <div>
+      “Amazing. Our team helped us build an app that delivered a new experience for 
+      hiring a physio. The launch was an instant success with 100k downloads in the 
+      first month.”
+      Arthur Clarke
+      Co-founder of MyPhysio
+    </div>
+  </div>
+</section>
+<section>
+  Ready to get started?
+  contact us
+</section>
